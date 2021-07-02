6c2abc5a614f09560565b8645454e77266dbde33
le4TC <anton.christenson@gmail.com>
2021-07-02 16:22:49 +0200

    new boards (total now 100)

--- boards.js
+++ boards.js
@@ -811,4 +811,194 @@ X X O X O O X X X
 . . . . . . X . .
 . . . . . . . . .
 
+. . O X X X . . .
+. . O O O X . . .
+. . . . O X . . .
+. . . . O X . . .
+. . O . O O X . .
+. . . O X X . . .
+. . . O X . X . .
+. . . O X X . . .
+. . . O O X . . .
+
+. . X O O . . . .
+. . X X O O . . .
+. . . . X O . . .
+. . . X X O . . .
+. . X . X X O . .
+. . . X O O . . .
+. . . X O . O . .
+. . . X O O . . .
+. . . X X O . . .
+
+. . O X X X . X .
+. . . O O X X . .
+. O O O O X O X X
+O . O X X X O X O
+X O O X O X O O O
+X O X X O O O . .
+X X X . X X O . .
+. . . . X O O O .
+. . . . X X O . .
+
+. . X O O . . O .
+. X . X O O O . .
+. X X X X O X O O
+X . X O O O X O X
+O X X O X O X X X
+O X O O X X X . .
+O O O . O O X . .
+. . . . O X X X .
+. . . . O O X . .
+
+. . . . O O X . .
+. . O . O X . X .
+O O . . O X . . .
+X O O O X X X X .
+X X X X O O X . .
+. . X O O . O X X
+. . X O . O . O X
+X . X X O . O . O
+. . X O O . . . .
+
+. . . . X X O . .
+. . X . X O . O .
+X X . X X O . . .
+O X X X O O O O .
+O O O O O X O . .
+. . O X X X X O O
+. . O X . X . X O
+O . O O X . X . X
+. . O X X . . . .
+
+. . . . . . . . .
+. . . . . . . . .
+O O . O . . . O .
+X O X O O O . . .
+X O O X O X O O O
+X X X X X X X X X
+. . . . X O O O .
+. . . X . X O . .
+. . . . X . . . .
+
+. . . . . . . . .
+. . . . . . . . .
+X X . X . . X X .
+O X O X X X . . X
+O X X O X O X X O
+O O O O O O O O O
+. . . . O X X X .
+. . . O . O X . .
+. . . . O . . . .
+
+. . . X X O . . .
+. . X . X O . O .
+. X X X O O . . .
+. . X O . . O O O
+X X X O O O X X O
+O X O X X X X . X
+O O O O O X . X .
+. . . . O O X . .
+. O . . O X X . .
+
+. . . O O O X . .
+. . O . O X . X .
+. . O O X X . . .
+. . O X . . X X X
+O O O X X X O O X
+X O X O O O O . O
+X X X X X O . O .
+. . . . X X O . .
+. X . . X O O . .
+
+. . . . . . . . .
+. . . . O O . . .
+. . O . O X O O .
+. . . O X X X O O
+O O O O X X O O X
+X O X O X . X O X
+X X X X . X . X X
+X O O O X X . X .
+. . O . O . . . .
+
+. . . . . . . . .
+. . . . X X . . .
+. . X . X O X X X
+. . . X O O O X O
+X X X X O O X X O
+O X O X O . O X O
+O O O O . O . O O
+O X X X O O . O .
+. X . X X . . . .
+
+O O O X X . O X .
+X X O O X X X . .
+X O O . O X O X X
+X X O O . O O X O
+X . X O . . . O O
+. X . X O . . O .
+. X X X O . . . .
+. . . X X O O . .
+. . . X O O . . .
+
+O X X O O . X O .
+O O X X O O O . .
+O X X . X O X O O
+O O X X . X X O X
+O . O X . . . X X
+. O . O X . . X .
+. . O O X . X . .
+. O X O O X . . .
+. . X O X X . . .
+
+. X X O . O X X .
+X . X O X O X . X
+. X O O O O O X X
+O . X O X O . O X
+. X X X X O O O O
+X X O X X X X O .
+X O O O X O O O .
+O . O O X X X O .
+. O . O X . X O .
+
+. O O X . X O O .
+O . O X . X O . O
+. O X X X X X O O
+X . O X O X . X O
+. O O O O X X X X
+O O X O O O O X .
+O X X X O X X X .
+X . X X O O O X .
+. X X O O . O X .
+
+. . . O O X . . .
+. O X O X . X . .
+. . X O X . . . .
+O O O O O X X . .
+X O O O X . X X .
+X O X X X X O X X
+X X X O O X O O X
+. X O . O O . O O
+. X O O . . . . .
+
+. . . X X O . . .
+. X O X O . O . .
+. . . X O . . . .
+X X X X X O O . .
+O X O X O . O O .
+O X O O O O X O O
+O O O X X O X X O
+X O X . X X . X X
+. O X X . . . . .
+
+. . X O O . . . .
+. X . X O . O . .
+. . . X O . . . .
+X X X X X O O . .
+O X O X O . O O O
+O X O O O O X O X
+O O O X X O X X X
+. O X . X X . X .
+. O X X . . . . .
+
 `.trim().split('\n\n')
